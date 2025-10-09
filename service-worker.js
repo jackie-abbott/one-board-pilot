64bdf85dc440cd60c3d50092ebabc97ecdfcfc3d
jackie-abbott <jkay.abbott@gmail.com>
2025-10-08 22:45:55 -0400

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,16 @@
+const CACHE = "one-board-pilot-v1";
+const ASSETS = ["/one-board-pilot/","/one-board-pilot/index.html"];
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+});
+self.addEventListener("fetch", e => {
+  const url = new URL(e.request.url);
+  if (url.origin === location.origin) {
+    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  }
+});
